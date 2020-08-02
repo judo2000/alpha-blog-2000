12b69aaca10da7e2a051f62a1cf08c8901b26aaa
Scott Moore <judo2000@comcast.net>
2020-08-01 22:38:25 -0600

    Add requirement for admin to create categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
-  
+  before_action :require_admin, except: [:index, :show]
+
   def new
     @category = Category.new
   end
@@ -26,5 +27,13 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.required(:category).permit(:name)
+  end
+
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
+    
   end
 end
--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  
+  setup do
+    @admin_user = User.create(username: "johndoe", email: "johndoe@example.com",
+                              password: "password", admin: true)
+    sign_in_as(@admin_user)
+  end
+  
   test "get new category form and create new category" do
     get "/categories/new"
     assert_response :success
